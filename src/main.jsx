7f2518014d21d043778162e3efce31f55b5d658a
jimmyurl <jimmy.james365@gmail.com>
2025-03-14 10:26:23 +0300

    updated version

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import UnderMaintenance from "./assets/UnderMaintenance"; 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+root.render(
+  <React.StrictMode>
+    <UnderMaintenance />
+  </React.StrictMode>
+);
